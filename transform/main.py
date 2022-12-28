53061ab8776553158144228fdb5dfae65aa24376
Vincent Faust <vincentffaust@gmail.com>
2022-12-27 19:45:50 -0600

    cosmetic fix

--- transform/main.py
+++ transform/main.py
@@ -1,4 +1,4 @@
-import datetime 
+import datetime
 
 from depositing import Deposit
 from withdrawing import Withdraw
@@ -10,55 +10,47 @@ from ordering import Order
 def main():
 
     today = datetime.datetime.utcnow()
-    yesterday = today - datetime.timedelta(days=1) 
+    yesterday = today - datetime.timedelta(days=1)
     today = today.strftime('%Y-%m-%d')
-    yesterday = yesterday.strftime('%Y-%m-%d') 
+    yesterday = yesterday.strftime('%Y-%m-%d')
 
-    #creating objects across deposits, withdrawals, mints, transfer and orders endpoints respectively 
-    
-    hro_deposits = Deposit(parameters = {'min_timestamp' : f'{yesterday}T00:00:00.00Z', 
-                                     'max_timestamp' : f'{yesterday}T23:59:59.99Z',
-                                    'token_address' : '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6', 
-                                     'direction' : 'asc'})
-        
-    hro_withdraws = Withdraw(parameters = {'min_timestamp' : f'{yesterday}T00:00:00.00Z', 
-                                     'max_timestamp' : f'{yesterday}T23:59:59.99Z',
-                                    'token_address' : '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6', 
-                                     'direction' : 'asc'})
-    
-    hro_creates = Mint(parameters = {'min_timestamp' : f'{yesterday}T00:00:00.00Z', 
-                                     'max_timestamp' : f'{yesterday}T23:59:59.99Z',
-                                     'token_address' : '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6', 
-                                     'direction' : 'asc'})
-    
-    hro_moves = Transfer(parameters = {'min_timestamp' : f'{yesterday}T00:00:00.00Z', 
-                                     'max_timestamp' : f'{yesterday}T23:59:59.99Z',
-                                     'token_address' : '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6',
-                                     'direction' : 'asc'})
-    
-    hro_orders = Order(parameters = {'min_timestamp' : f'{yesterday}T00:00:00.00Z', 
-                                        'max_timestamp' : f'{yesterday}T23:59:59.99Z',
-                                        'order_by' : 'timestamp', 
-                                        'direction' : 'asc',
-                                        'sell_token_address' : '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6'
-                                        },
-                                     today=today, 
-                                     yesterday=yesterday)
-    
-  
-    print(hro_deposits.df)   
+    # creating objects across deposits, withdrawals, mints, transfer and orders endpoints respectively
+
+    hro_deposits = Deposit(parameters={'min_timestamp': f'{yesterday}T00:00:00.00Z',
+                                       'max_timestamp': f'{yesterday}T23:59:59.99Z',
+                                       'token_address': '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6',
+                                       'direction': 'asc'})
+
+    hro_withdraws = Withdraw(parameters={'min_timestamp': f'{yesterday}T00:00:00.00Z',
+                                         'max_timestamp': f'{yesterday}T23:59:59.99Z',
+                                         'token_address': '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6',
+                                         'direction': 'asc'})
+
+    hro_creates = Mint(parameters={'min_timestamp': f'{yesterday}T00:00:00.00Z',
+                                   'max_timestamp': f'{yesterday}T23:59:59.99Z',
+                                   'token_address': '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6',
+                                   'direction': 'asc'})
+
+    hro_moves = Transfer(parameters={'min_timestamp': f'{yesterday}T00:00:00.00Z',
+                                     'max_timestamp': f'{yesterday}T23:59:59.99Z',
+                                     'token_address': '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6',
+                                     'direction': 'asc'})
+
+    hro_orders = Order(parameters={'min_timestamp': f'{yesterday}T00:00:00.00Z',
+                                   'max_timestamp': f'{yesterday}T23:59:59.99Z',
+                                   'order_by': 'timestamp',
+                                   'direction': 'asc',
+                                   'sell_token_address': '0x8cb332602d2f614b570c7631202e5bf4bb93f3f6'
+                                   },
+                       today=today,
+                       yesterday=yesterday)
+
+    print(hro_deposits.df)
     print(hro_withdraws.df)
     print(hro_creates.df)
     print(hro_moves.df)
     print(hro_orders.df)
 
 
-
-
-
-
-
 if __name__ == '__main__':
     main()
-
-
